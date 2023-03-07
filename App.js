e59647dcf9aeaabe338a3e747b8438a4b0328d45
Ari Bengiyat <ari@aribengiyat.com>
2023-03-07 18:40:23 -0500

    DynamoDB working

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
-import { Homepage } from './jhgfds/Homepage';
+import { Homepage } from './routes/Homepage';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import { Amplify, Auth, Hub } from 'aws-amplify';
 import config from "./src/aws-exports";
