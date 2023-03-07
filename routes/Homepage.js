147fb8f3506983d22efb473adb96a8dc248467ed
Meg Schneider <megschneider616@gmail.com>
2023-03-07 12:21:32 -0500

    add modal for settings

--- routes/Homepage.js
+++ routes/Homepage.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Alert, Modal, StyleSheet, Pressable } from 'react-native';
 // import { styles } from '../App';
 import styled from 'styled-components/native';
+import { Settings } from './Settings';
 
 const Title = styled.Text`
   color: red;
@@ -10,22 +11,31 @@ const Title = styled.Text`
 
 export const Homepage = () => {
   const [weatherData, setWeatherData] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=40.748004761173796&lon=-73.9972450826255&appid=9d078590b75f76a8f744905541a91990&units=metric`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setWeatherData(data);
-      });
+    setWeatherData({ main: { temp: '25°' } });
+    // fetch(
+    //   `https://api.openweathermap.org/data/2.5/weather?lat=40.748004761173796&lon=-73.9972450826255&appid=9d078590b75f76a8f744905541a91990&units=metric`
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     setWeatherData(data);
+    //   });
   }, []);
 
   return (
     <View>
-      <Title>Current Temperature: </Title>
+      <Title>fun.shine</Title>
       <Title>{weatherData.main ? weatherData.main.temp : '...loading'} </Title>
+      {modalVisible ? <Settings/> : null}
+      <Pressable
+        // style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(!modalVisible)}>
+          <Text>Show Modal</Text>
+        {/* <Text style={styles.textStyle}>Show Modal</Text> */}
+      </Pressable>
     </View>
   );
 };
---
+++ routes/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+export  function Settings() {
+  // const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View >
+     
+              <Text>Hide Modal</Text>
+
+    </View>
+  );
+};
