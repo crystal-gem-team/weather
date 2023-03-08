6dadff22067b75874ce255b1f60f7a8cdafba395
dnvt <francois@dnvt.me>
2023-03-07 19:28:03 -0500

    fixed weird Routes folder git issue

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
-import { Homepage } from './Routes/Homepage';
+import { Homepage } from './path/Homepage';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import { Amplify, Auth, Hub } from 'aws-amplify';
 import config from './src/aws-exports';
